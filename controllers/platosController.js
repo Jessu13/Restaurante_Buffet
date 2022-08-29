47984e6047624c095e587000b8d3a64051d7fff4
PipeCano23 <80290418+pipeCano23@users.noreply.github.com>
2022-08-29 14:52:23 -0500

    Update platosController.js

--- controllers/platosController.js
+++ controllers/platosController.js
@@ -2,12 +2,12 @@ import { Plato } from "../models/Plato.js";
 
 const guardar_platos = (req, res) => {
     //Validar
-    const { nombre, descripcion, precio } = req.body;
+    const { nombreplato, descriptplato, precioplato } = req.body;
     try {
-        Plato.create({
-            nombre,
-            precio,
-            descripcion
+        await Plato.create({
+            nombre: nombreplato, 
+            precio: precioplato,
+            descripcion: descriptplato, 
         });
         res.redirect('/platos');
     } catch (error) {
@@ -15,4 +15,40 @@ const guardar_platos = (req, res) => {
     }
 }
 
-export { guardar_platos };
+const eliminar_plato = async (req, res) => {
+    //Validar
+    const { nombreplato} = req.body;
+    try {
+        await Plato.destroy({
+            where: { 
+                nombre: nombreplato 
+            },
+        });
+        res.redirect('/platos');
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const actualizar_plato = async (req, res) => {
+    //Validar
+    const { nombreplato, descriptplato, precioplato} = req.body;
+    try {
+        await User.update(
+            {
+                nombre: nombreplato, 
+                precio: precioplato,
+                descripcion: descriptplato,
+            },
+            {
+                where: { nombre: nombreplato },
+            }
+        );
+        res.redirect('/platos');
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+export { guardar_platos, eliminar_plato, actualizar_plato };
